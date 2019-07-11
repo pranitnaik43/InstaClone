e0f44d28642f05aac3f4d8633e65a82da9fbf12f
rey <pranitnaikk43@gmail.com>
2019-07-11 14:25:30 +0530

    Get Data from parse server

--- app/src/main/java/com/pdevelopers/instaclone/MainActivity.java
+++ app/src/main/java/com/pdevelopers/instaclone/MainActivity.java
@@ -2,10 +2,12 @@ package com.pdevelopers.instaclone;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.parse.FindCallback;
 import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
@@ -14,10 +16,13 @@ import com.parse.ParseQuery;
 import com.parse.SaveCallback;
 import com.shashank.sony.fancytoastlib.FancyToast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity{
 
     private Button nextBtn, saveBtn, viewBtn;
     private EditText bname, punchSpeed;
+    private String allBoxers;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         try {
@@ -43,7 +48,7 @@ public class MainActivity extends AppCompatActivity{
                         @Override
                         public void done(ParseException e) {
                             if (e == null) {
-                                FancyToast.makeText(MainActivity.this, "Boxer Saved", FancyToast.LENGTH_LONG, FancyToast.SUCCESS, true).show();
+                                FancyToast.makeText(MainActivity.this, "Boxer Saved", FancyToast.LENGTH_SHORT, FancyToast.SUCCESS, true).show();
                                 bname.setText("");
                                 punchSpeed.setText("");
                             } else {
@@ -56,13 +61,34 @@ public class MainActivity extends AppCompatActivity{
             viewBtn.setOnClickListener(new View.OnClickListener(){
                 @Override
                 public void onClick(View v) {
+                    //################## Get a single object ############################3
+//                    ParseQuery<ParseObject> query = ParseQuery.getQuery("MyCustomClassName");
+//                    query.getInBackground("<PARSE_OBJECT_ID>", new GetCallback<ParseObject>() {
+//                        public void done(ParseObject result, ParseException e) {
+//                            if (e == null) {
+//                                System.out.println(result);
+//                            } else {
+//                                // something went wrong
+//                            }
+//                        }
+//                    });
+
+//                    ########### Get a list of objects  #####################3
                     ParseQuery<ParseObject> query = ParseQuery.getQuery("Boxer");
-                    query.getInBackground("<PARSE_OBJECT_ID>", new GetCallback<ParseObject>() {
-                        public void done(ParseObject result, ParseException e) {
+
+                    query.findInBackground(new FindCallback<ParseObject>() {
+                        @Override
+                        public void done(List<ParseObject> objects, ParseException e) {
                             if (e == null) {
-                                System.out.println(result);
+                                if(objects.size()>0){
+                                    allBoxers="";
+                                    for(ParseObject boxer : objects){
+                                        allBoxers=allBoxers+boxer.get("BoxerName")+" "+boxer.get("PunchSpeed")+"\n";
+                                    }
+                                    FancyToast.makeText(MainActivity.this,allBoxers,FancyToast.LENGTH_SHORT,FancyToast.DEFAULT,true).show();
+                                }
                             } else {
-                                // something went wrong
+                                FancyToast.makeText(MainActivity.this, e.getMessage(), FancyToast.LENGTH_LONG, FancyToast.ERROR, true).show();
                             }
                         }
                     });
